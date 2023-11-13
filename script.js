5826ee9e6d17143ff7826b9da676296f915c5c25
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-11-14 00:43:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let first= document.getElementById("first");
+let last =document.getElementById("last");
+let phone = document.getElementById("phone");
+let email=document.getElementById("email");
+let button=document.getElementsByTagName("button");
