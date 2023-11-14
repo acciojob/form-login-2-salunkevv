bee122cc5f4c375fe41eec947bdf5a68718cc1c2
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-11-14 16:15:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-//your JS code here. If required.
-let first= document.getElementById("first");
-let last =document.getElementById("last");
+let first = document.getElementById("first");
+let last = document.getElementById("last");
 let phone = document.getElementById("phone");
-let email=document.getElementById("email");
-let button=document.getElementsByTagName("button");
+let email = document.getElementById("email");
+let buttons = document.getElementById("btn");
+console.log(buttons.value)
+function func()
+{
+    console.log(first.value,last.value,phone.value,email.value);
+    alert(`First Name: ${first.value} \nLast Name: ${last.value} \nPhone Number: ${phone.value} \nEmail Id: ${email.value}`)
+}
+buttons.addEventListener("click",func);
